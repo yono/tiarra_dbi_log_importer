8254feecd07f9e457cbeef09da0b55f853cd208c
yono <yono.05@gmail.com>
2011-08-01 00:18:22 +0900

    fix logic to find config file

--- lib/model/model.py
+++ lib/model/model.py
@@ -3,9 +3,11 @@
 
 import ConfigParser
 from datetime import datetime
+import os
 import MySQLdb
 
-CONFIG_INI = "../../config.ini"
+FILE_DIR = os.path.dirname(os.path.abspath(__file__))
+CONFIG_INI = os.path.join(FILE_DIR, "../..", "config.ini")
 
 class Model(object):
 
@@ -18,21 +20,26 @@ class Model(object):
 
     @classmethod
     def connect(self):
+        if self.user is None:
+            self.read_conf()
         if self.con is None:
             self.con = MySQLdb.connect(user=self.user, passwd=self.passwd, 
                                       db=self.db, charset=self.charset,
                                       use_unicode=True)
             self.cur = self.con.cursor()
 
+    @classmethod
+    def read_conf(self):
+        conf = ConfigParser.SafeConfigParser()
+        conf.read(CONFIG_INI)
+        
+        self.user = conf.get('db', 'username')
+        self.passwd = conf.get('db', 'password')
+        self.db = conf.get('db', 'db')
+        self.charset = conf.get('db', 'charset')
+
     def __init__(self):
-        if user is None:
-            conf = ConfigParser.SafeConfigParser()
-            conf.read(CONFIG_INI)
-            
-            self.user = conf.get('db', 'username')
-            self.passwd = conf.get('db', 'password')
-            self.db = conf.get('db', 'db')
-            self.charset = conf.get('db', 'charset')
+        pass
 
     def str_to_datetime(self, date, time):
         return datetime.strptime(date + 'T' + time + 'Z', "%Y.%m.%dT%H:%M:%SZ")
